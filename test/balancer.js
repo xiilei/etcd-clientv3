bce9f5d251bde80a3994fadc86c3090e4b90f194
xiilei <xilei125@163.com>
2016-07-21 20:47:27 +0800

    balancer.get always get same endpoint if next not be called

--- test/balancer.js
+++ test/balancer.js
@@ -5,9 +5,11 @@ describe('balancer',function () {
     it('#get',function(){
         var balancer = new Balancer([1,2,3]);
         expect(balancer.get()).to.equal(1);
-        expect(balancer.get()).to.equal(2);
-        expect(balancer.get()).to.equal(3);
         expect(balancer.get()).to.equal(1);
+        balancer.next();
+        expect(balancer.get()).to.equal(2);
+        balancer.next();
+        expect(balancer.get()).to.equal(3);
     });
     it('only one endpoint',function(){
         var balancer = new Balancer([1]);
